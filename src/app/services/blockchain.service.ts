575d8b27ce11231e92175949c3373b1d018e1b34
rmaleryk <seaker000@gmail.com>
2018-06-19 16:37:44 +0300

    Review comments were fixed

--- src/app/services/blockchain.service.ts
+++ src/app/services/blockchain.service.ts
@@ -11,6 +11,7 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class BlockchainService {
   private progressSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  private isLoaded = false;
 
   get progress() {
     return this.progressSubject.asObservable();
@@ -34,8 +35,6 @@ export class BlockchainService {
   }
 
   private loadBlockchainBlocks() {
-    let isLoaded = false;
-
     this.checkConnectionState().subscribe(
       () => {
         this.ngZone.runOutsideAngular(() => {
@@ -43,29 +42,14 @@ export class BlockchainService {
           .create(90000)
           .startWith(1)
           .flatMap(() => this.getBlockchainProgress())
-          .takeWhile((response: any) => !response.current || !isLoaded)
+          .takeWhile((response: any) => !response.current || !this.isLoaded)
           .subscribe(
-            response => {
-              this.ngZone.run(() => {
-                this.progressSubject.next(response);
-
-                if (response.current === response.highest) {
-                  isLoaded = true;
-                  this.completeLoading();
-                }
-              });
-            },
-            () => {
-              throw ConnectionError.UNAVAILABLE_BACKEND;
-            }
+            response => this.onBlockchainProgress(response),
+            () => { throw new Error(ConnectionError.UNAVAILABLE_BACKEND); }
           );
         });
       },
-      (error: any) => this.onLoadBlockchainError(
-        !!ConnectionError[error]
-        ? error
-        : ConnectionError.UNAVAILABLE_BACKEND
-      )
+      (error: Error) => this.onLoadBlockchainError(error)
     );
   }
 
@@ -73,19 +57,31 @@ export class BlockchainService {
     return this.apiService.get('blockchain/progress');
   }
 
+  private onBlockchainProgress(response: any) {
+    this.ngZone.run(() => {
+      this.progressSubject.next(response);
+
+      if (response.current === response.highest) {
+        this.completeLoading();
+      }
+    });
+  }
+
   private completeLoading() {
+    this.isLoaded = true;
     this.walletService.loadBalances();
   }
 
-  private onLoadBlockchainError(error: ConnectionError) {
-    this.progressSubject.next({ isError: true, error: error });
+  private onLoadBlockchainError(error: Error) {
+    const connectionError = ConnectionError[error.message] || ConnectionError.UNAVAILABLE_BACKEND;
+    this.progressSubject.next({ isError: true, error: connectionError });
   }
 
   private checkConnectionState(): Observable<ConnectionError> {
     return this.apiService.get('network/connections')
       .map(response => {
         if (response.connections.length === 0) {
-          throw ConnectionError.NO_ACTIVE_CONNECTIONS;
+          throw new Error(ConnectionError.NO_ACTIVE_CONNECTIONS);
         }
 
         return null;
